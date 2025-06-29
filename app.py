424044cbc17517eaffc0702372424f1b05ab41fe
Herset-Taiwan <dennis068899@gmail.com>
2025-06-29 12:47:30 +0000

    fix render startCommand

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # 請確認 templates/index.html 存在
+    return render_template("index.html")
 
-# 不要加 app.run()，Render 會自動使用 gunicorn 啟動這個 app
+if __name__ == '__main__':
+    app.run()
