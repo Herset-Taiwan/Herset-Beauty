8b9ed15c52d9bf37206d4af926232b00bbb96d7b
Herset-Taiwan <dennis068899@gmail.com>
2025-07-02 03:30:22 +0000

    新增會員系統

--- static/script.js
+++ static/script.js
@@ -1,8 +1,4 @@
-document.querySelectorAll("button").forEach(btn => {
-    btn.addEventListener("click", () => {
-        alert("商品已加入購物車（僅前端示意）");
-    });
-});
+
 function openProfileModal() {
   const modal = document.getElementById("profile-modal");
   if (modal) {
@@ -41,3 +37,5 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 });
+
+
