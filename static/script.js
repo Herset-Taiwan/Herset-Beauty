9d648f295412b13ca0d27bd228ed1280a02c60ed
Herset-Taiwan <dennis068899@gmail.com>
2025-08-03 06:32:03 +0000

    商品規格

--- static/script.js
+++ static/script.js
@@ -66,3 +66,13 @@ function confirmCloseProfile() {
   document.getElementById("profile-modal").style.display = "none";
 }
 
+let currentCategory = null;
+
+function filterCategory(categoryName) {
+  currentCategory = categoryName;
+  // 清除目前商品、重新載入
+  document.querySelector("#product-list").innerHTML = '';
+  fetch(`/api/products?category=${encodeURIComponent(categoryName)}`)
+    .then(res => res.json())
+    .then(products => renderProducts(products));
+}
